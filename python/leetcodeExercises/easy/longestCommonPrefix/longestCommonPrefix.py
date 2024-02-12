317f454fab6dddf89d456f51011b7c3c519f3513
Ben Clarke <benthedev@outlook.com>
2024-02-12 23:40:23 +0000

    Update longestCommonPrefix.py

--- python/leetcodeExercises/easy/longestCommonPrefix/longestCommonPrefix.py
+++ python/leetcodeExercises/easy/longestCommonPrefix/longestCommonPrefix.py
@@ -1,4 +1,4 @@
-# TODO create a passing and failing test for this in test.py
+# TODO create a passing and a failing test for this in test.py
 def longestCommonPrefix(self, strs: List[str]) -> str:
     """
     Finds the longest common prefix from a given list of words
