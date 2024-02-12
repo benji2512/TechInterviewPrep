299a0accfb3655fa2e366b1d5afca56507055f73
Ben Clarke <benthedev@outlook.com>
2024-02-12 22:49:51 +0000

    nonsense update

--- python/leetcodeExercises/easy/longestCommonPrefix/test.py
+++ python/leetcodeExercises/easy/longestCommonPrefix/test.py
@@ -1,2 +1,11 @@
 # @todo Write tests for thise
 # @body Use pytest to write a passing and failing test for this
+
+class TestTwoSum(unittest.TestCase):
+  def setUp(self):
+    self.solution = twoSum.Solution
+  def test_Passing1(self):
+    self.assertEqual(self.solution.twoSum([2,7,11,15], 9), [0,1])
+
+if __name__ == "__main__":
+  unittest.main
